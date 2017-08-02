8b8c09538db8de94e2d25557c9ef9743863960b9
zhouguohui <zhouguohui1029@163.com>
2017-08-02 19:55:56 +0800

    Init project

--- src/Parser/ParameterParser.php
+++ src/Parser/ParameterParser.php
@@ -41,19 +41,35 @@ class ParameterParser {
 					$parameter->getDefaultValueConstantName() : $parameter->getDefaultValue()) : '';
 				$isOptional = $parameter->isOptional();
 				$isReference = $parameter->isPassedByReference();
+				$isArray = $parameter->isArray();
+				$isCall = $parameter->isCallable();
 				$class = $parameter->getClass();
 				$item = [
 					'name'        => '$' . $parameter->getName(),
 					'position'    => $parameter->getPosition(),
-					'default'     => $default,
+					'default'     => $isOptional ? ($isArray ? [] : ($isCall ? NULL : $default)) : $default,
 					'is_optional' => $isOptional,
 					'modify'      => !$class ? '' : ($this->useNamespace ? '\\' . $class->getName() : $class->getShortName()),
 				];
+				var_dump($item);
 				!$isReference ?: $item['name'] = '&' . $item['name'];
 				$this->args[] = $item;
 			}
 		}
 		array_multisort(array_column($this->args, 'position'), SORT_ASC, $this->args);
+	}
+	
+	protected function defaultValueToString($value) {
+		switch(TRUE) {
+			case is_null($value):
+				return 'NULL';
+			case is_array($value):
+				return '[]';
+			case is_string($value):
+				return '\'' . $value . '\'';
+			default:
+				return $value;
+		}
 	}
 	
 	/**
@@ -62,7 +78,11 @@ class ParameterParser {
 	public function __toString() {
 		$argStr = [];
 		foreach($this->args as $arg) {
-			$argStr[] = ($arg['modify'] ? $arg['modify'] . ' ' : '') . $arg['name'] . ($arg['default'] ? '=' . $arg['default'] : '');
+			if($arg['is_optional']) {
+				$argStr[] = ($arg['modify'] ? $arg['modify'] . ' ' : '') . $arg['name'] . ' = ' . $this->defaultValueToString($arg['default']);
+			} else {
+				$argStr[] = ($arg['modify'] ? $arg['modify'] . ' ' : '') . $arg['name'];
+			}
 		}
 		
 		return implode(', ', $argStr);
